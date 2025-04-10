17253c79328b8c9d0a7cedb9a0ccb783061a14cb
Sahil Bajaj <bajaj@Sahils-MacBook-Pro-2.local>
2025-04-10 23:44:03 +0530

    run demo script

---
+++ run_demo.sh
@@ -0,0 +1,10 @@
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/2/1.jpg --text-prompt "fabrics. furniture. wood and metal trim finishes. walls. windows. rugs. flooring. any accessories."
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/2/2.jpg --text-prompt "fabrics. furniture. wood and metal trim finishes. walls. windows. rugs. flooring. any accessories."
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/2/3.jpg --text-prompt "fabrics. furniture. wood and metal trim finishes. walls. windows. rugs. flooring. any accessories."
+
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/3/3_1.jpg --text-prompt "model. hair. flesh. shirt. jacket. pant. shoe. bag., background. dress."
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/3/3_2.jpg --text-prompt "model. hair. flesh. shirt. jacket. pant. shoe. bag., background. dress."
+python grounded_sam2_hf_model_demo.py --img-path notebooks/images/3/3_3.jpg --text-prompt "model. hair. flesh. shirt. jacket. pant. shoe. bag., background. dress."
+
+
+
